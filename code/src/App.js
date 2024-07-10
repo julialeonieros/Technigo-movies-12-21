94181e76d106336a9d3cd073827035945d463679
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2024-07-10 15:37:58 +0200

    Fix eslint error

--- code/src/App.js
+++ code/src/App.js
@@ -1,14 +1,13 @@
-import React from "react";
-import { MovieList } from "./pages/MovieList";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import MovieDetails from "pages/MovieDetails";
+import MovieDetails from 'pages/MovieDetails';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList } from './pages/MovieList';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={MovieList} />
-        <Route path="/:movieID" exact component={MovieDetails} />
+        <Route path='/' exact component={MovieList} />
+        <Route path='/:movieID' exact component={MovieDetails} />
       </Switch>
     </BrowserRouter>
   );
