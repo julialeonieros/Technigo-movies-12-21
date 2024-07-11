369ac895cb3ea2ecfd35e6cf85fef3366fddbaa3
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2024-07-11 08:51:04 +0200

    Revert "Fix eslint error"
    This reverts commit 94181e76d106336a9d3cd073827035945d463679.

--- code/src/App.js
+++ code/src/App.js
@@ -1,13 +1,14 @@
-import MovieDetails from 'pages/MovieDetails';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { MovieList } from './pages/MovieList';
+import React from "react";
+import { MovieList } from "./pages/MovieList";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import MovieDetails from "pages/MovieDetails";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact component={MovieList} />
-        <Route path='/:movieID' exact component={MovieDetails} />
+        <Route path="/" exact component={MovieList} />
+        <Route path="/:movieID" exact component={MovieDetails} />
       </Switch>
     </BrowserRouter>
   );
